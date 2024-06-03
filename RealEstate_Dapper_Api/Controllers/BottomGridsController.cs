81dde89dd5f7dbec9613b71f95bdc905442ecf70
cananbuyukdagg <canan@dabnet.com.tr>
2024-06-03 21:27:44 +0300

    development cb

---
+++ RealEstate_Dapper_Api/Controllers/BottomGridsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.BottomGridRepositories;
+using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BottomGridsController : ControllerBase
+    {
+        private readonly IBottomGridRepository _bottomGridrepository;
+        public BottomGridsController(IBottomGridRepository bottomGridRepository)
+        {
+            _bottomGridrepository = bottomGridRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> BottomGridList()
+        {
+            var values = await _bottomGridrepository.GetAllBottomGridAsync();
+            return Ok(values);
+        }
+    }
+ 
+}
