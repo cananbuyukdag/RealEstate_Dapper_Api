6fb1a0925fb769288a83ed6a68611b7559d80807
cananbuyukdagg <canan@dabnet.com.tr>
2024-06-04 23:19:46 +0300

    development

---
+++ RealEstate_Dapper_Api/Repositories/TestimonialRepositories/ITestimonialRepository.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate_Dapper_Api.Dtos.ServiceDtos;
+using RealEstate_Dapper_Api.Dtos.TestimoialsDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.TestimonialRepositories
+{
+    public interface ITestimonialRepository
+    {
+        Task<List<ResultTestimonialDto>> GetAllTestimonialAsync();
+    }
+}
