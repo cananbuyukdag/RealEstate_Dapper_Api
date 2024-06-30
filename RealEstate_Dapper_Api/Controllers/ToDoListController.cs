ef65a873fc0ea37784856c0691e6abd9bde2df48
cananbuyukdagg <canan@dabnet.com.tr>
2024-07-01 00:03:49 +0300

    .

---
+++ RealEstate_Dapper_Api/Controllers/ToDoListController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.ToDoListRepositories;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoListController : ControllerBase
+    {
+        private readonly IToDoListRepository _ToDoListRepository;
+        public ToDoListController(IToDoListRepository ToDoListRepository)
+        {
+            _ToDoListRepository = ToDoListRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ToDoListList()
+        {
+            var values = await _ToDoListRepository.GetAllToDoListAsync();
+            return Ok(values);
+        }
+    }
+}
